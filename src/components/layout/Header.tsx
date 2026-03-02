a024f6bcc82e81cc51b322a34dec35c1374975a6
KyeongBin Min <min06150315@gmail.com>
2026-03-02 02:18:45 +0000

    Tailwind CSS 세팅

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <>
-      <div>Header</div>
+    <header>
+      <div className="bg-brand">Header</div>
       <Link to="/">Home</Link>
       <Link to="/Blog">Blog</Link>
       <Link to="/Project">Project</Link>
-    </>
+    </header>
   );
 }
 
