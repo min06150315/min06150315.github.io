3b427a93c310382cae431bf190b6944aac42f873
KyeongBin Min <min06150315@gmail.com>
2026-03-02 01:44:03 +0000

    Supabase 백엔드 연결

---
+++ src/api/posts.api.ts
@@ -0,0 +1,24 @@
+import { supabase } from '@/lib/supabase';
+import type { Post } from '@/types';
+
+export const getPosts = async (): Promise<Post[]> => {
+  const { data, error } = await supabase
+    .from('posts')
+    .select('*')
+    .order('created_at', { ascending: false });
+
+  console.log(data);
+  if (error) throw new Error(error.message);
+  return data as Post[];
+};
+
+export const getPostById = async (id: string | number): Promise<Post> => {
+  const { data, error } = await supabase
+    .from('posts')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) throw new Error(error.message);
+  return data as Post;
+};
