d4d30ddbebb53a43cb6581d275cf2537db345c19
KyeongBin Min <min06150315@gmail.com>
2026-03-07 03:55:06 +0000

    코드 다듬기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
-function App() {
+const App = () => {
   return (
     <>
       <RouterProvider router={router} />
     </>
   );
-}
+};
 
 export default App;
