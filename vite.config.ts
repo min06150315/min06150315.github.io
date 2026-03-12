10ca7811191ab6d2d6922f64b73b2b07bbfd11e1
KyeongBin Min <min06150315@gmail.com>
2026-03-12 13:27:33 +0000

    gh-pages 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // 타입 에러 실시간 확인
 import checker from 'vite-plugin-checker';
@@ -6,15 +6,19 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      checker({
+        typescript: true,
+      }),
+    ],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    base: env.VITE_APP_BASE_URL || '/',
+  };
 });
