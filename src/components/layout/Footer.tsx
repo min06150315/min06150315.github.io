2dcac6d7ce4351a0b9bc7d40b640c2ee0dbeb865
KyeongBin Min <min06150315@gmail.com>
2026-04-26 14:40:59 +0000

    푸터 내용 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,30 +1,54 @@
-import { SOCIAL_LINKS } from '@/constants/links';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="w-full border-t border-slate-500 py-12">
-      <div className="max-w-5xl mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-y-4">
-          <p className="text-sm font-bold">
-            © 2026 minbin blog. Powerd by React, Supabase, Github Pages.
-          </p>
-
-          <div className="flex gap-6">
-            {SOCIAL_LINKS.map((link) => (
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center gap-y-4 px-4">
+        <p className="text-sm text-gray-400">
+          © 2026 mindev. All rights reserved.
+        </p>
+        <address className="not-italic text-center md:text-right text-gray-500 text-xs leading-relaxed">
+          <p>
+            Powered by{' '}
+            <span className="text-gray-400">
               <Link
-                key={link.name}
-                to={link.link}
-                title={link.name}
+                className="hover:text-primary"
+                to="https://react.dev/"
                 target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-base-gray"
               >
-                <link.icon />
+                React
               </Link>
-            ))}
-          </div>
-        </div>
+              ,{' '}
+              <Link
+                className="hover:text-primary"
+                to="https://supabase.com/"
+                target="_blank"
+              >
+                Supabase
+              </Link>
+              ,{' '}
+              <Link
+                className="hover:text-primary"
+                to="https://docs.github.com/pages"
+                target="_blank"
+              >
+                GitHub Pages
+              </Link>
+            </span>
+          </p>
+          <p>
+            Design by{' '}
+            <span className="text-gray-400">
+              <Link
+                className="hover:text-primary"
+                to="https://stitch.withgoogle.com/"
+                target="_blank"
+              >
+                Google Stitch
+              </Link>
+            </span>
+          </p>
+        </address>
       </div>
     </footer>
   );
