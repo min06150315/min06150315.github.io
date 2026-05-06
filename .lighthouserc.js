fc4806244e420698ac917006f0e3f902513abdce
KyeongBin Min <min06150315@gmail.com>
2026-05-06 14:21:17 +0000

    Lighthouse 경고 수준 명시

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,6 +7,15 @@ export default {
         chromeFlags: '--no-sandbox --disable-setuid-sandbox --headless',
       },
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
